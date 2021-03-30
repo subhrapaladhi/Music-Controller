9f835934cb83e24a62f91c7aec383597755016f8
subhrapaladhi <subhrapaladhi9@gmail.com>
2021-03-30 20:49:39 +0530

    join room page

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -6,13 +6,17 @@ import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 
 export default class App extends Component {
-    constructor(props){
-        super(props);       
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render(){
-        return <HomePage />
-    }
+  render() {
+    return (
+      <div className="center">
+        <HomePage />
+      </div>
+    );
+  }
 }
 
 const appDiv = document.getElementById("app");
